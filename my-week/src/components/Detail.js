57240da6ec67fb26bfed249db52c2a432da0b7f3
hongsoom <hongsoom@kakao.com>
2022-05-21 13:47:49 +0900

    Initial page

---
+++ my-week/src/components/Detail.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Detail = (props) => {
+    return (
+        <h1>상세페이지</h1>
+    )
+}
+export default Detail;
---
+++ my-week/src/components/Score.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Score = (props) => {
+    return (
+        <h1>평균페이지</h1>
+    )
+}
+export default Score;
