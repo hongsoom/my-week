896e26c4b6018beb47d55f3c5cb5513324174f68
hongsoom <hongsoom@kakao.com>
2022-05-21 14:19:18 +0900

    route setting

--- my-week/src/App.js
+++ my-week/src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import {Route,Routes} from "react-router-dom";
+import React from "react";
+import Main from "./components/Main";
+import Detail from './components/Detail';
+import Score from './components/Score';
 
 function App() {
   return (
     <div className="App">
-  
+      <Routes>
+        <Route exact path="/" component={Main}/>
+        <Route exact path="/Detail" component={Detail} />
+        <Route exact path="/Score" component={Score} />
+      </Routes>
     </div>
   );
 }
