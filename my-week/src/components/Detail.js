2e91770348cabe8ed7bfa9257d2f55e38420e9dd
hongsoom <hongsoom@kakao.com>
2022-05-23 21:04:22 +0900

    useParams

--- my-week/src/components/Detail.js
+++ my-week/src/components/Detail.js
@@ -1,22 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 import { useParams, useHistory } from "react-router-dom";
 
 
 const Detail = (props) => {
     const week = props.week
-    const { index } = useParams();
+    const index = useParams();
+    console.log(index)
+    
     const history = useHistory();
+
+    const [circle, setCircle] = useState([0, 0, 0, 0, 0])
     
     const onClick = () => {
-        history.goBack('/')
+        history.push('/')
     } 
+
     return (
         <div>
-            <h3>{week[index]} 평점 남기기</h3>
-            <div></div>
+            <h3>{week[index.id]} 평점 남기기</h3>
+            {circle.map((num, index) => {
+                return (
+                    <Circle key={index} style={{backgroundColor : index ?  'green' : 'yellow'}}></Circle>
+                )
+            })
+            }
             <button onClick={onClick}>평점 남기기</button>
         </div>
     )
 }
+
+const Circle = styled.div`
+  width: 30px;
+  height: 30px;
+  border-radius: 30px;
+  margin: 20px 10px;
+  border: 1px solid black;
+`;
+
 export default Detail;
