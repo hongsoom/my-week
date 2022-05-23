4e57cc17f8756a0b2c2a0ba248abb58c0a2906c2
hongsoom <hongsoom@kakao.com>
2022-05-23 21:06:22 +0900

    change css

--- my-week/src/components/Score.js
+++ my-week/src/components/Score.js
@@ -5,21 +5,12 @@ const Score = (props) => {
     const {rateAverage, removeRate} = props;
     return (
         <div>
-            <InputWrap>
-                <h3>평균 평점</h3>
-                <p>{rateAverage}</p>
-                <button onClick={removeRate}>Reset</button>
-            </InputWrap>
+            <h3>평균 평점</h3>
+            <p>{rateAverage}</p>
+            <button onClick={removeRate}>Reset</button>
         </div>    
     )
 }
-const InputWrap = styled.div`
-  background-color: #fff;
-  width: 30vw;
-  max-width: 350px;
-  margin: auto;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-`;
+
 
 export default Score;
