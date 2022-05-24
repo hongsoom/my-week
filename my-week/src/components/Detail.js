b26812933d64943f9129a97cf781d9e879613691
hongsoom <hongsoom@kakao.com>
2022-05-24 16:39:53 +0900

    change status when button clicked

--- my-week/src/components/Detail.js
+++ my-week/src/components/Detail.js
@@ -9,32 +9,55 @@ const Detail = (props) => {
     const { id } = useParams();
     const history = useHistory();
 
-    const [circle, setCircle] = useState([0, 0, 0, 0, 0])
+    const [circle, setCircle] = useState([1, 2, 3, 4, 5]);
+    const [click, setClick] = useState();
+
+    console.log(click)
     
     const onClick = () => {
         history.push('/')
     } 
 
     return (
-        <div>
+        <Container>
             <h3>{week[id]}요일 평점 남기기</h3>
-            {circle.map((num, index) => {
+            <CircleWarp>
+            {circle.map((circle, i) => {
                 return (
-                    <Circle key={index} style={{backgroundColor : index ?  'green' : 'yellow'}}></Circle>
+                    <Circle key={i} onClick={ () => setClick(i)} 
+                    style={{backgroundColor : click >= i ?  'yellow' : ('#D3D3D3')}}>
+                    </Circle>
                 )
-            })
-            }
+            })}
+            </CircleWarp>
             <button onClick={onClick}>평점 남기기</button>
-        </div>
+        </Container>
     )
 }
+
+const Container = styled.div`
+  max-width: 350px;
+  min-height: 80vh;
+  padding: 16px;
+  margin: 20px auto;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+  text-align: center;
+`;
+
+const CircleWarp = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px;
+`;
 
 const Circle = styled.div`
   width: 30px;
   height: 30px;
   border-radius: 30px;
   margin: 20px 10px;
-  border: 1px solid black;
+  cursor : pointer;
 `;
 
 export default Detail;
